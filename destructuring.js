3070a3556adbb5deb86032fec48d38ea671fc93d
Cat <ccallcottstevens@gmail.com>
2021-04-04 10:11:20 +1000

    destructing to pass an object as a functions params

--- destructuring.js
+++ destructuring.js
@@ -43,3 +43,48 @@ let a, b;
 //notice the brackets! without the brackets the {a, b} is considered a block and not an object literal
 ({a, b} = {a: 1, b: 2});
 
+//destructuring from nested objects
+const LOCAL_FORECAST = {
+    today: { min: 72, max: 83 },
+    tomorrow: { min: 73.3, max: 84.6 }
+  };
+  
+  function getMaxOfTmrw(forecast) {
+    "use strict";
+  // destructured twice
+    const {tomorrow : { max : maxOfTomorrow  }} = forecast; 
+  
+    return maxOfTomorrow;
+  }
+  
+  console.log(getMaxOfTmrw(LOCAL_FORECAST)); 
+
+  //destructuring from array - the difference is we can't choice which variables take on the new values eg: 
+  const [z, x ] = [1, 2, 3, 4, 5, 6]; //will go in order and z = 1 and x =2
+  //we can use empty spaces with commas to change the element that takes the new value
+  const [z, x, , y] = [1, 2, 3, 4, 5, 6];
+    console.log(z, x, y); // 1,2,4
+
+//Destructuring assignment to pass an object as a function's parameters
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+  };
+  const half = (function() {
+  //here we are passing the entire object - stats into the half function but we only need the min and max values
+  //  return function half(stats) {
+   //return (stats.max + stats.min) / 2.0;
+   // instead we can destructure the min and max from the object 
+   return function half({max, min }) {
+    return (max + min) / 2.0;
+   };
+  
+  })();
+  console.log(stats); 
+  console.log(half(stats)); 
+
+
