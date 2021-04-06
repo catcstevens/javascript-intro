abaae75479ef729f46a06c15f7caeebd409f6dde
Cat <ccallcottstevens@gmail.com>
2021-04-06 21:34:25 +1000

    hoisting and scope

---
+++ hoisting.js
@@ -0,0 +1,45 @@
+// Temporal deadzone
+
+// Global scope
+// The easiest way to make a variable global is by defining outside of any other scopes
+var globalVar = 'I am global!';
+
+function concatString(str) {
+  return str + ' ' + globalVar;
+}
+console.log(concatString('I am hungry!')); // 'I am hungry! I am global!
+
+// Scope Challenge
+
+const vegetable = 'carrot';
+let value;
+
+if(hoisted) {
+  var fruit = 'apple';
+}
+
+if(!hoisted) {
+  var fruit = 'banana';
+}
+
+function setString(str) {
+  window.str = str;
+}
+function addTenToNum(num) {
+  var ten = 10;
+  value = ten + num;
+}
+
+setString('Hello World');
+addTenToNum(5);
+
+var str = window.str;
+
+console.log('1: ', hoisted);
+console.log('2: ', fruit);
+console.log('3: ', value);
+console.log('4: ' , vegetable);
+console.log('5: ', str);
+console.log('6: ', ten);
+
+var hoisted = true;
--- syntacticSugar.js
+++ syntacticSugar.js
@@ -10,3 +10,26 @@ function makeClass() {
   const Vegetable = makeClass();
   const carrot = new Vegetable('carrot');
   console.log(carrot.name); 
+
+  //getters and setters
+
+  function makeClass() {
+    class Thermostat {
+      constructor(temp) {
+        this._temp = 5/9 * (temp - 32);
+      }
+      get temperature(){
+        return this._temp;
+      }
+      set temperature(updatedTemp){
+        this._temp = updatedTemp;
+      }
+    }
+    return Thermostat;
+  }
+  
+  const Thermostat = makeClass();
+  const thermos = new Thermostat(76); 
+  let temp = thermos.temperature; 
+  thermos.temperature = 26;
+  temp = thermos.temperature; 
