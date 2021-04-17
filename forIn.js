32d9bf1841ad526bb9c2334be22bb55877b2cef1
Cat <ccallcottstevens@gmail.com>
2021-04-17 12:37:56 +1000

    reduce method

--- forIn.js
+++ forIn.js
@@ -1,7 +1,20 @@
-function loopOverObject(object){
-    for (let key in object) {
-        console.log( `Key: ${key}; Value: ${object[key]}`)
-    }
+// function loopOverObject(object){
+//     for (let key in object) {
+//         console.log( `Key: ${key}; Value: ${object[key]}`)
+//     }
+// }
+
+// loopOverObject({foo: 'bar', ding: 'dong'})
+
+function staircase(n) {
+    
+      
+        for (let i = 1; i <= n; i++) {
+            console.log((' '.repeat(n-i)) + ('#'.repeat(i)))
+           
+        }
+    
+      
 }
 
-loopOverObject({foo: 'bar', ding: 'dong'})
+staircase(6)
